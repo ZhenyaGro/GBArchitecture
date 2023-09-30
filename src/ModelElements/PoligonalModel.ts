fbb6b36256651e09048b3a3cbcd8ba8a902c2173
ZhenyaGro <bon4ios@gmail.com>
2023-09-30 17:56:21 +0300

    PoligonalModel class

--- src/ModelElements/PoligonalModel.ts
+++ src/ModelElements/PoligonalModel.ts
@@ -1,3 +1,12 @@
-export class PoligonalModel {
+import { Texture } from "./Texture";
+import { Poligon } from "./Poligon";
 
+export class PoligonalModel {
+  public poligons: Array<Poligon>;
+  public textures: Array<Texture>;
+
+  constructor(textures: Array<Texture>) {
+    this.textures = textures;
+    this.poligons = new Array<Poligon>();
+  }
 }
