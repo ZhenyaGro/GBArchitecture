1023aa957c6c21ce78de42d70866d535644ea5c9
ZhenyaGro <bon4ios@gmail.com>
2023-09-30 18:52:48 +0300

    Flash class

--- src/ModelElements/Flash.ts
+++ src/ModelElements/Flash.ts
@@ -1,3 +1,23 @@
-export class Flash {
+import { Point3D } from "../Stuff/Point3D";
+import { Angle3D } from "../Stuff/Angle3D";
+import { Color } from "../Stuff/Color";
+import { Scene } from "./Scene";
 
+export class Flash {
+  public location: Point3D;
+  public angle: Angle3D;
+  public color: Color;
+  public power: number;
+  public scenes: Array<Scene>;
+
+  constructor(location: Point3D, angle: Angle3D, color: Color, power: number, scenes: Array<Scene>) {
+    this.location = location;
+    this.angle = angle;
+    this.color = color;
+    this.power = power;
+    this.scenes = scenes;
+  }
+
+  public rotate(angle: Angle3D): void { };
+  public move(point: Point3D): void { };
 }
