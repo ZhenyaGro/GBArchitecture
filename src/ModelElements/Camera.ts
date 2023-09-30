92847d927932f1b04c9e26a7d1a8eb173314de3e
ZhenyaGro <bon4ios@gmail.com>
2023-09-30 18:57:10 +0300

    Camera class

--- src/ModelElements/Camera.ts
+++ src/ModelElements/Camera.ts
@@ -1,3 +1,15 @@
-export class Camera {
+import { Point3D } from "../Stuff/Point3D";
+import { Angle3D } from "../Stuff/Angle3D";
 
+export class Camera {
+  public location: Point3D;
+  public angle: Angle3D;
+
+  constructor(location: Point3D, angle: Angle3D) {
+    this.location = location;
+    this.angle = angle;
+  }
+
+  public rotate(angle: Angle3D): void { };
+  public move(point: Point3D): void { };
 }
