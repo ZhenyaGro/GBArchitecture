7178e5b5615c11217e1ce805f8b82e665f86970f
ZhenyaGro <bon4ios@gmail.com>
2023-09-30 23:57:23 +0300

    Remove files for seminar 2

--- public/scripts/index.js
+++
@@ -1,8 +0,0 @@
-import { ModelStore } from "./InMemoryModel/ModelStore";
-class Test {
-    ApplyUpdateModel() {
-        throw new Error("Method not implemented.");
-    }
-}
-const changeObservers = new Array(new Test());
-const modelStore = new ModelStore(changeObservers);
