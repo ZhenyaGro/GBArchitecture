88e790d202ed5d0ca9c8871fd5654f9064fffd3a
ZhenyaGro <bon4ios@gmail.com>
2023-09-30 20:55:33 +0300

    Add push one element

--- src/ModelElements/PoligonalModel.ts
+++ src/ModelElements/PoligonalModel.ts
@@ -1,5 +1,6 @@
 import { Texture } from "./Texture";
 import { Poligon } from "./Poligon";
+import { Point3D } from "../Stuff/Point3D";
 
 export class PoligonalModel {
   public poligons: Array<Poligon>;
@@ -8,5 +9,8 @@ export class PoligonalModel {
   constructor(textures: Array<Texture>) {
     this.textures = textures;
     this.poligons = new Array<Poligon>();
+
+    // В соответствии со схемой добавляется один полигон
+    this.poligons.push(new Poligon(new Point3D()));
   }
 }
