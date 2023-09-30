5eb600f86d35ee0ccb65b86284610f50e1cbf7f2
ZhenyaGro <bon4ios@gmail.com>
2023-09-30 21:08:03 +0300

    Add arrays length checkers

--- src/ModelElements/Scene.ts
+++ src/ModelElements/Scene.ts
@@ -9,17 +9,22 @@ export class Scene {
   public cameras: Array<Camera>;
 
   constructor(id: number, models: Array<PoligonalModel>, flashes: Array<Flash>, cameras: Array<Camera>) {
+    if (models.length == 0)
+      throw new Error('Должна быть хотябы одна модель');
+    if (cameras.length == 0)
+      throw new Error('Должна быть хотябы одна камера');
+
     this.id = id;
     this.models = models;
     this.flashes = flashes;
     this.cameras = cameras;
   }
 
-  public method1<T>(parameter: T): T {
-    return parameter;
+  public method1<T>(flash: T): T {
+    return flash;
   }
 
-  public method2<T, K>(parameter1: T, parameter2: K): K {
-    return parameter2;
+  public method2<T, K>(model: T, flash: K): T {
+    return model;
   }
 }
