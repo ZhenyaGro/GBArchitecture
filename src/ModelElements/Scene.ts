d743a6df832375645b9705a7ecfa039ac37ae747
ZhenyaGro <bon4ios@gmail.com>
2023-09-30 18:42:12 +0300

    Scene class

--- src/ModelElements/Scene.ts
+++ src/ModelElements/Scene.ts
@@ -1,3 +1,25 @@
+import { PoligonalModel } from "./PoligonalModel";
+import { Flash } from "./Flash";
+import { Camera } from "./Camera";
+
 export class Scene {
   public id: number;
+  public models: Array<PoligonalModel>;
+  public flashes: Array<Flash>;
+  public cameras: Array<Camera>;
+
+  constructor(id: number, models: Array<PoligonalModel>, flashes: Array<Flash>, cameras: Array<Camera>) {
+    this.id = id;
+    this.models = models;
+    this.flashes = flashes;
+    this.cameras = cameras;
+  }
+
+  public method1<T>(parameter: T): T {
+    return parameter;
+  }
+
+  public method2<T, K>(parameter1: T, parameter2: K): K {
+    return parameter2;
+  }
 }
